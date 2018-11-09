13a4e8bd9cbe28fe3dfa5b338b23c7c66cd6758f
John Castillo <john.gnu@gmail.com>
2018-11-09 13:57:21 -0400

    table_demo

---
+++ tabla_demo.sql
@@ -0,0 +1,11 @@
+﻿DROP TABLE public."TrnPartDefuncionDemo";
+CREATE TABLE public."TrnPartDefuncionDemo"
+(
+  "IdCiudadano" integer NOT NULL,
+  "IdNroOficialia" integer,
+  "FechaInscripcion" date NOT NULL,
+  "NombresFallecido" character varying(100),
+  "PaisInscripcion" integer NOT NULL,
+  "IdLocInscripcion" integer NOT NULL,
+  CONSTRAINT "TrnPartDefuncionDemo_pkey" PRIMARY KEY ("IdCiudadano")
+)
